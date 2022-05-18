b090e6fbfd7878afc67847cab8bd2cee013e5426
miroslaw <miroslawm@ecom.house>
2022-05-18 11:19:56 +0200

    Fix

--- src/Domain/Service/DhlApi.php
+++ src/Domain/Service/DhlApi.php
@@ -4,7 +4,7 @@ namespace EcomHouse\DeliveryPoints\Domain\Service;
 
 class DhlApi implements SpeditorInterace
 {
-    public function getPoints(string $token, ?string $postCode = null)
+    public function getPoints(string $token, ?int $page = 1, ?int $perPage = 25)
     {
         // TODO: Implement getPoints() method.
     }
