b269fcc15e16b4d83399fd07baaa8b62cfeefe88
miroslaw <miroslawm@ecom.house>
2022-05-19 10:35:55 +0200

    dhl - add api_url

--- src/Domain/Service/DhlApi.php
+++ src/Domain/Service/DhlApi.php
@@ -4,6 +4,8 @@ namespace EcomHouse\DeliveryPoints\Domain\Service;
 
 class DhlApi implements SpeditorInterace
 {
+    private const API_URL = 'https://api-gw.dhlparcel.nl/';
+
     public function getPoints(string $token, ?int $page = 1, ?int $perPage = 25)
     {
         // TODO: Implement getPoints() method.
