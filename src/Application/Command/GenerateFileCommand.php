2164af41ad8d1eca88d1a5cd37b32076bbfe56b7
miroslaw <miroslawm@ecom.house>
2022-06-01 14:12:52 +0200

    correcting execute

--- src/Application/Command/GenerateFileCommand.php
+++ src/Application/Command/GenerateFileCommand.php
@@ -9,28 +9,22 @@ use EcomHouse\DeliveryPoints\Domain\Helper\FileExtension;
 
 class GenerateFileCommand implements GenerateFileCommandInterface
 {
-    protected array $config;
-    protected CsvBuilder $csvBuilder;
-    protected XmlBuilder $xmlBuilder;
+    private array $dataBuilder = [];
 
     public function __construct(array $config = [])
     {
-        $this->config = $config;
-        $this->csvBuilder = new CsvBuilder;
-        $this->xmlBuilder = new XmlBuilder;
+        foreach ($config as $param) {
+            $this->dataBuilder[] = match ($param) {
+                FileExtension::XML => new XmlBuilder,
+                FileExtension::CSV => new CsvBuilder
+            };
+        }
     }
 
     public function execute($data, string $filename)
     {
-        foreach ($this->config as $param) {
-            switch ($param) {
-                case FileExtension::XML:
-                    $this->xmlBuilder->build($filename, $data, []);
-                    break;
-                case FileExtension::CSV:
-                    $this->csvBuilder->build($filename, $data, DeliveryPointFactory::getHeaders());
-                    break;
-            }
+        foreach ($this->dataBuilder as $dataBuilder) {
+            $dataBuilder->build($filename, $data, DeliveryPointFactory::getHeaders());
         }
     }
 }
