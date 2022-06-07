f046e032559232ce4d7952c38b426bc1e0ab7f1c
miroslaw <miroslawm@ecom.house>
2022-06-07 15:17:01 +0200

    unit test for ConnectorUri

---
+++ tests/Infrastructure/Connector/ConnectorUriTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Infrastructure\Connector;
+
+use EcomHouse\DeliveryPoints\Infrastructure\Connector\ConnectorUri;
+use PHPUnit\Framework\TestCase;
+
+class ConnectorUriTest extends TestCase
+{
+    protected ConnectorUri $connectorUri;
+
+    protected function setUp(): void
+    {
+        $this->connectorUri = new ConnectorUri;
+    }
+
+    public function testDoRequest()
+    {
+        $url = 'https://placowki.poczta-polska.pl/pliki-owp.php?t=csvK48S';
+        $filename = $this->connectorUri->doRequest($url);
+
+        $this->assertFileExists($filename);
+        unlink($filename);
+    }
+}
